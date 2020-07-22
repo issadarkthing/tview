ccb67418e2a08f518929e98b1c8b7c717bfc55dd
raziman <raziman.terra@gmail.com>
2020-07-22 21:41:59 +0800

    fix suspend error

--- application.go
+++ application.go
@@ -280,15 +280,17 @@ func (a *Application) Run() error {
 				return
 			}
 
-			// We have a new screen. Keep going.
-			a.Lock()
-			a.screen = screen
-			a.Unlock()
 
 			// Initialize and draw this screen.
 			if err := screen.Init(); err != nil {
 				panic(err)
 			}
+
+			// We have a new screen. Keep going.
+			a.Lock()
+			a.screen = screen
+			a.Unlock()
+
 			a.draw()
 		}
 	}()
